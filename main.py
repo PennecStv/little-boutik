17e1297ee6ee213e4f58615cd5a2950ce037d8b9
Steve Pennec <steve.pennec@outlook.fr>
2024-12-05 15:20:30 +0100

    feat: add endpoint to get sales from a customer (challenge 3)

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ async def get_customers():
     # Make a request to the Hiboutik API with Basic Auth
     response = httpx.get(base_url + "/customers", auth=(api_login, api_key))
     if response.status_code == 200:
-        data = response.json()
-        return {"data": data}
+        customers = response.json()
+        return customers
     else:
         return {"error": response.status_code, "message": response.json()}
    
@@ -33,8 +33,8 @@ async def get_customers():
 async def get_customer_by_id(customer_id: int):
     response = httpx.get(base_url + "/customer/" + customer_id, auth=(api_login, api_key))
     if response.status_code == 200:
-        data = response.json()
-        return {"last_name": data["last_name"], "first_name": data["first_name"], "email": data["email"]}
+        customer = response.json()
+        return [{"last_name": customer["last_name"], "first_name": customer["first_name"], "email": customer["email"]}]
     else:
         return {"error": response.status_code, "message": response.json()}
 
@@ -45,21 +45,31 @@ async def get_customer(last_name: str = None, email: str = None):
     response = httpx.get(base_url + "/customers", auth=(api_login, api_key))
     
     if response.status_code == 200: 
-        data = response.json()
+        customers = response.json()
         if last_name and email:
-            return [customer for customer in data if last_name.lower() 
+            return [customer for customer in customers if last_name.lower() 
                     in customer["last_name"].lower() or last_name.lower() 
                     in customer["first_name"].lower() and email.lower() 
                     in customer["email"].lower()]
         elif last_name:
-            return [customer for customer in data if last_name.lower() 
+            return [customer for customer in customers if last_name.lower() 
                     in customer["last_name"].lower() or last_name.lower() 
                     in customer["first_name"].lower()]
         elif email:
-            return [customer for customer in data if email.lower() 
+            return [customer for customer in customers if email.lower() 
                     in customer["email"].lower()]
         else:
             return {"error": "Please provide a last name or email"}
     else:
         return {"error": response.status_code, "message": response.json()}
         
+        
+@app.get("/customer/{customer_id}/sales")
+async def get_customer_sales(customer_id: int):
+    response = httpx.get(base_url + "/customer/" + str(customer_id) + "/sales", auth=(api_login, api_key))
+    
+    if response.status_code == 200:
+        customer_sales = response.json()
+        return customer_sales
+    else:
+        return {"error": response.status_code, "message": response.json()}
