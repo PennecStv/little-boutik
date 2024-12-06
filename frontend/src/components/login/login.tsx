7d6bb532fbcce34b4a1fd2fdfea5d93cdbe98e26
Steve Pennec <steve.pennec@outlook.fr>
2024-12-06 10:47:39 +0100

    feat: enhanced login page by adding a password field to access (challenge 8)

--- frontend/src/components/login/login.tsx
+++ frontend/src/components/login/login.tsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { NavigateFunction, useNavigate } from 'react-router-dom';
 import './login.css';
-import { Button, TextField } from '@mui/material';
+import { Button, Snackbar, SnackbarCloseReason, SnackbarContent, TextField } from '@mui/material';
 import LoginIcon from '@mui/icons-material/Login';
+import bcrypt from "bcryptjs-react";
 
 interface MyLoginProps {
   navigate: NavigateFunction;
@@ -10,25 +11,67 @@ interface MyLoginProps {
 
 interface MyLoginState {
   username: string;
+  password: string;
+  snackbarOpen: boolean;
+  snackbarMessage: string;
+  snackbarVariant: 'success' | 'error';
 }
+
+// Pre-hash the "admin" password
+// Has to modified to use the actual password hash from the database
+const adminHashedPassword = bcrypt.hashSync('admin', 10);
 
 class MyLogin extends Component<MyLoginProps, MyLoginState> {
   constructor(props: MyLoginProps) {
     super(props);
     this.state = {
       username: '',
+      password: '',
+      snackbarOpen: false,
+      snackbarMessage: '',
+      snackbarVariant: 'success',
     };
   }
 
-  handleLogin = () => {
-    this.props.navigate('/customers');
+  handleLogin = async () => {
+    const { username, password } = this.state;
+    
+    if (username === 'admin' && bcrypt.compareSync(password, adminHashedPassword)) {
+      this.setState({
+        snackbarOpen: true,
+        snackbarMessage: 'Successfully logged in',
+        snackbarVariant: 'success',
+      });
+      this.props.navigate('/customers');
+    } else {
+      this.setState({
+        snackbarOpen: true,
+        snackbarMessage: 'Invalid login or password',
+        snackbarVariant: 'error',
+      });
+    }
   };
 
   handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ username: event.target.value });
+    this.setState({ [event.target.name]: event.target.value } as unknown as Pick<MyLoginState, keyof MyLoginState>);
+  };
+
+  handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      this.handleLogin();
+    }
+  };
+
+  handleSnackbarClose = (event: React.SyntheticEvent | Event, reason?: SnackbarCloseReason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    this.setState({ snackbarOpen: false });
   };
 
   render() {
+    const {  snackbarMessage, snackbarVariant } = this.state;
+
     return (
       <div className='container login'>
         <div className="header">
@@ -37,14 +80,34 @@ class MyLogin extends Component<MyLoginProps, MyLoginState> {
           </div>
         </div>
         <div className="inputs">
-          <TextField id="outlined-basic" label="Login" variant="outlined" 
+          <TextField id="outlined-basic" label="Login" variant="outlined" type='text' name='username'
             value={this.state.username} 
             onChange={this.handleChange} 
+          />
+          <TextField id="outlined-basic" label="Password" variant="outlined" type="password" name='password'
+            value={this.state.password} 
+            onChange={this.handleChange}
+            onKeyDown={this.handleKeyPress} 
           />
           <Button variant="contained" onClick={this.handleLogin} startIcon={<LoginIcon />}>
             Enter
             </Button>
         </div>
+        <Snackbar
+        open={this.state.snackbarOpen}
+        autoHideDuration={3000}
+        onClose={this.handleSnackbarClose}
+        message={this.state.snackbarMessage}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      >
+        <SnackbarContent
+            message={snackbarMessage}
+            style={{
+              backgroundColor: snackbarVariant === 'success' ? 'green' : 'red',
+            }}
+          />
+      </Snackbar>
+      
       </div>
     );
   }
